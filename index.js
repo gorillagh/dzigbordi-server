ca2becae4c1fd7305749482bcd447e1edd3b03dc
Albert <ron468896@gmail.com>
2023-06-30 08:34:59 +0000

    first commit

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ mongoose
   );
 
 //Middlewares
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   })
+// );
 
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "6000mb" })); //Used to parse JSON bodies
